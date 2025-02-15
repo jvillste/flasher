8bcfa1d21e613362a499d06f43e95198bea2359a
Jukka Villstedt <jukka.villstedt@iki.fi>
2025-02-15 06:54:46 +0200

    add uberjar settings

--- project.clj
+++ project.clj
@@ -1,10 +1,13 @@
-(defproject flasher "0.1.0-SNAPSHOT"
+(defproject flasher "1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.12.0"]
-                 [flow-gl/flow-gl "3"]
+                 [flow-gl/flow-gl "4"]
                  [medley "1.3.0"]
                  [clojure.java-time "0.3.3"]]
-  :repl-options {:init-ns flasher.core})
+  :repl-options {:init-ns flasher.core}
+  :main flasher.times-table
+  :profiles {:uberjar {:aot :all}}
+  :clean-targets ^{:protect false} ["targets"])
